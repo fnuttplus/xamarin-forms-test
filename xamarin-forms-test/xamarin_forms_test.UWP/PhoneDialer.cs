c7c1c857a698d5a6db314929e385cc01ee8be214
fnuttplus <fnuttplus@evry>
2016-04-06 15:51:31 +0200

    Init

---
+++ xamarin-forms-test/xamarin_forms_test.UWP/PhoneDialer.cs
@@ -0,0 +1,18 @@
+﻿
+using Phoneword.WinPhone;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace Phoneword.WinPhone
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+
+            
+            return true;
+        }
+    }
+}
